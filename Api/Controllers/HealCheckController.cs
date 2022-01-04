c179156e4e3be6bce5bb099e8a4b51e53da8af19
Artem Kolokolov <yougrysha@gmail.com>
2022-01-05 01:55:03 +0300

    added healthcheck

---
+++ Api/Controllers/HealCheckController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("health")]
+    public class HealCheckController : Controller
+    {
+        [HttpGet]
+        public IActionResult Check()
+        {
+            return NoContent();
+        }
+    }
+}
